bd72a342811d02cc0a2d357d952b44e9bd8fed40
shaofuping <731912314@qq.com>
2018-12-21 11:46:18 +0800

    添加zuul路由过滤

---
+++ springcloud-eurekademo/springcloudzuul-api-gateway/src/main/java/com/anleafworld/springcloudzuulapigateway/filter/AccessFilter.java
@@ -0,0 +1,42 @@
+package com.anleafworld.springcloudzuulapigateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AccessFilter extends ZuulFilter {
+    private Logger logger = LoggerFactory.getLogger(AccessFilter.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        logger.info("send {} request to {}", request.getMethod(), request.getRequestURL().toString());
+        Object accessToken = request.getParameter("accessToken");
+        if (accessToken == null) {
+            logger.warn("accessToken is empty");
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(401);
+            return null;
+        }
+        logger.info("accessToken is ok");
+        return null;
+    }
+}
