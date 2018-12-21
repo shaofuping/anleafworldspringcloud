f6bb4c10bc19762763e16325eae233e970136c1e
xinxiang <menghuanjiutian@163.com>
2018-12-21 08:31:40 +0800

    zuul project commit

---
+++ springcloud-eurekademo/springcloudzuul-api-gateway/src/main/java/com/anleafworld/springcloudzuulapigateway/SpringcloudzuulApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.anleafworld.springcloudzuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class SpringcloudzuulApiGatewayApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SpringcloudzuulApiGatewayApplication.class).web(true).run(args);
+    }
+
+}
+
