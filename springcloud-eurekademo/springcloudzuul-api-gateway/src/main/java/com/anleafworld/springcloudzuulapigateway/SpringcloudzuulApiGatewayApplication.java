05c466b029d37ec76b4f22718cd0e5c0a228f9b8
shaofuping <731912314@qq.com>
2018-12-21 11:28:41 +0800

    git

--- springcloud-eurekademo/springcloudzuul-api-gateway/src/main/java/com/anleafworld/springcloudzuulapigateway/SpringcloudzuulApiGatewayApplication.java
+++ springcloud-eurekademo/springcloudzuul-api-gateway/src/main/java/com/anleafworld/springcloudzuulapigateway/SpringcloudzuulApiGatewayApplication.java
@@ -3,10 +3,12 @@ package com.anleafworld.springcloudzuulapigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class SpringcloudzuulApiGatewayApplication {
 
     public static void main(String[] args) {
